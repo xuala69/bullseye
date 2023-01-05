a78d99304de57b6635e32452deaadbbee2b42593
Xuala khawlhring <synxuala@gmail.com>
2023-01-05 15:33:40 +0530

    update ui

--- lib/UI/home.dart
+++ lib/UI/home.dart
@@ -1,5 +1,6 @@
 import 'dart:math';
 
+import 'package:bullseye/utilities/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
@@ -32,89 +33,261 @@ class _HomePageState extends State<HomePage> {
     setState(() {});
   }
 
+  void hitmeClick() {
+    final sliderValueRounded = (sliderValue * 100).round();
+    final difference = (sliderValueRounded - targetValue).abs();
+    final scoredPoints = (100 - difference);
+
+    setState(() {});
+    showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: Text(
+                "Slider value is $sliderValueRounded and the target is $targetValue. Point scored is $scoredPoints"),
+            actions: [
+              TextButton(
+                  onPressed: () {
+                    currentScore = currentScore + scoredPoints;
+                    currentRound++;
+                    generateRandomInt();
+                    Navigator.of(context).pop();
+                  },
+                  child: const Text("OK"))
+            ],
+          );
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Image.asset(
-            "assets/bullseye.jpeg",
-            width: 70,
-            height: 70,
+        body: Stack(
+      children: [
+        Image.asset(
+          "assets/bullseye.jpeg",
+          width: double.infinity,
+          fit: BoxFit.fitWidth,
+        ),
+        Container(
+          width: double.infinity,
+          height: double.infinity,
+          decoration: BoxDecoration(
+            color: const Color.fromARGB(255, 99, 44, 23).withOpacity(0.99),
           ),
-          const SizedBox(
-            height: 10,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Text(
-                "Drag the slider to get as close as you can to: ",
-                style: TextStyle(
-                  fontSize: 18,
-                ),
-              ),
-              Text(
-                "$targetValue",
-                style: const TextStyle(
-                  fontSize: 30,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ],
-          ),
-          const SizedBox(
-            height: 25,
-          ),
-          Slider(
-              value: sliderValue,
-              onChanged: (newValue) {
-                sliderValue = newValue;
-                setState(() {});
-              }),
-          const SizedBox(
-            height: 25,
-          ),
-          ElevatedButton(
-            onPressed: () {
-              final difference =
-                  ((sliderValue * 100).round() - targetValue).abs();
-              currentScore = currentScore + (100 - difference);
-              currentRound++;
-              setState(() {});
-              generateRandomInt();
-            },
-            child: const Text("Hit me!"),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(left: 20, right: 20),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        ),
+        Column(
+          children: [
+            const Spacer(),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                ElevatedButton(
-                  onPressed: () {
-                    currentRound = 1;
-                    currentScore = 0;
-                    setState(() {});
-                  },
-                  child: const Text("Start over"),
+                const Text(
+                  "Put the bullseye as close as you can to: ",
+                  style: TextStyle(
+                    fontSize: 24,
+                    color: Colors.white,
+                  ),
                 ),
-                Text("Score: $currentScore"),
-                Text("Round: $currentRound"),
-                ElevatedButton(
-                  onPressed: () {
-                    final difference =
-                        (sliderValue * 100).round() - targetValue;
-                    print("DIfference is $difference");
-                  },
-                  child: const Text("Score board"),
+                Text(
+                  "$targetValue",
+                  style: const TextStyle(
+                    fontSize: 40,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.yellow,
+                  ),
                 ),
               ],
             ),
-          )
-        ],
-      ),
-    );
+            const SizedBox(
+              height: 25,
+            ),
+            Row(
+              children: [
+                const SizedBox(
+                  width: 25,
+                ),
+                const Text(
+                  "1",
+                  style: TextStyle(
+                    fontSize: 24,
+                    color: Colors.white,
+                  ),
+                ),
+                Expanded(
+                  child: Slider(
+                    value: sliderValue,
+                    activeColor: Colors.green,
+                    thumbColor: Colors.white,
+                    onChanged: (newValue) {
+                      sliderValue = newValue;
+                      setState(() {});
+                    },
+                  ),
+                ),
+                const Text(
+                  "100",
+                  style: TextStyle(
+                    fontSize: 24,
+                    color: Colors.white,
+                  ),
+                ),
+                const SizedBox(
+                  width: 25,
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 25,
+            ),
+            ElevatedButton(
+              onPressed: hitmeClick,
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Constants.buttonBackground,
+                shape: const RoundedRectangleBorder(
+                  side: BorderSide(
+                    color: Colors.black,
+                    width: 1.5,
+                  ),
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(7),
+                  ),
+                ),
+                padding:
+                    const EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+              ),
+              child: Text(
+                Constants.hitmeText,
+                style: const TextStyle(
+                  fontSize: 18,
+                  color: Colors.black,
+                ),
+              ),
+            ),
+            const Spacer(),
+            Padding(
+              padding: const EdgeInsets.only(left: 20, right: 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
+                      currentRound = 1;
+                      currentScore = 0;
+                      setState(() {});
+                      generateRandomInt();
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Constants.buttonBackground,
+                      shape: const RoundedRectangleBorder(
+                        side: BorderSide(
+                          color: Colors.black,
+                          width: 1.5,
+                        ),
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(7),
+                        ),
+                      ),
+                    ),
+                    child: Row(
+                      children: [
+                        const Icon(
+                          Icons.refresh,
+                          color: Colors.black,
+                        ),
+                        const SizedBox(
+                          width: 5,
+                        ),
+                        Text(
+                          Constants.startOverText,
+                          style: const TextStyle(
+                            color: Colors.black,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Row(
+                    children: [
+                      const Text(
+                        "Score:",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 16,
+                        ),
+                      ),
+                      Text(
+                        "$currentScore",
+                        style: const TextStyle(
+                          color: Colors.yellow,
+                          fontSize: 21,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      const Text(
+                        "Round:",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 16,
+                        ),
+                      ),
+                      Text(
+                        "$currentRound",
+                        style: const TextStyle(
+                          color: Colors.yellow,
+                          fontSize: 21,
+                        ),
+                      ),
+                    ],
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      final difference =
+                          (sliderValue * 100).round() - targetValue;
+                      print("DIfference is $difference");
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Constants.buttonBackground,
+                      shape: const RoundedRectangleBorder(
+                        side: BorderSide(
+                          color: Colors.black,
+                          width: 1.5,
+                        ),
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(7),
+                        ),
+                      ),
+                    ),
+                    child: Row(
+                      children: [
+                        const Icon(
+                          Icons.info_outline,
+                          color: Colors.black,
+                        ),
+                        const SizedBox(
+                          width: 5,
+                        ),
+                        Text(
+                          Constants.infoText,
+                          style: const TextStyle(
+                            color: Colors.black,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            )
+          ],
+        ),
+      ],
+    ));
   }
 }
---
+++ lib/utilities/constants.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class Constants {
+  static String hitmeText = "Hit me!";
+  static String startOverText = "Start over";
+  static String infoText = "info";
+  static Color buttonBackground = const Color(0xFFC58940);
+}
